0d02c480f64e0bef8a304093dafa3f8c35fd331f
spittaya <spittaya@uwo.ca>
2018-03-31 12:02:42 -0400

    Intial Se Debugging
    corrected ka and changed the naming convention of Se. everything for Se is now correct.

---
+++ Project/Shaft Design/Test.m
@@ -0,0 +1,6 @@
+ka = ka_func(560,2) %ka with Sut at 560 MPa and machined finish (2)
+kb = kb_func(43,1) %kb with 43mm diameter and bending load
+kc = kc_func(1) %bending loading
+kd = kd_func(25)
+ke = ke_func(0.9)
+Se_prime = Se_prime_func(560)
--- Project/Shaft Design/ka_func.m
+++ Project/Shaft Design/ka_func.m
@@ -6,6 +6,6 @@ function [ka] = ka_func(Sut, finish)
 %  3 - Hot-rolled
 %  4 - As-forged
 table = [1.58 -0.085;4.51 -0.265;57.7 -0.718;272 -0.995];
-ka = (table(finish,1)*Sut)^table(finish,2);
+ka = table(finish,1)*(Sut^table(finish,2));
 end
 
