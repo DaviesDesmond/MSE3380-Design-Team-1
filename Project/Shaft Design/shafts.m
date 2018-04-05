9b49b3ca6754acb14416a27f7f35d7cb7646ac80
spittaya <spittaya@uwo.ca>
2018-04-05 18:56:56 -0400

    Calculating Shaft FOS
    for the most complicated shaft, didn't change code, performed calculations only.

--- Project/Shaft Design/shafts.m
+++ Project/Shaft Design/shafts.m
@@ -2,15 +2,25 @@ function [FOS,FOS_torsion,FOS_yield] = shafts(Sut,finish,d,loading,temp,reliabil
 %UNTITLED9 Summary of this function goes here
 %   Detailed explanation goes here
 Se = se_func(Sut,finish,d,loading,temp,reliability);
+
 Kf_bend = Kf_func(Kt_bend,q_bend);
+
 Kf_axial = Kf_func(Kt_axial,q_axial);
+
 Kfs = Kfs_func(Kts,qs);
+
 [sigma_a_bend,sigma_m_bend]=amp_median_calc(sigma_bending_max,sigma_bending_min);
+
 [sigma_a_axial,sigma_m_axial]=amp_median_calc(sigma_axial_max,sigma_axial_min);
+
 [Ta,Tm]=amp_median_calc(torque_max,torque_min);
+
 siga_prime = sigma_a_prime_func(Kf_bend,Kf_axial,sigma_a_bend,sigma_a_axial,Kfs,Ta);
+
 sigm_prime = sigma_m_prime_func(Kfs,Kf_axial,sigma_m_bend,sigma_m_axial,Kfs,Tm);
-[FOS,FOS_torsion]=modGoodman_fos_func(siga_prime,sigm_prime,Ta,Tm,Se,Sut)
+
+[FOS,FOS_torsion] = modGoodman_fos_func(siga_prime,sigm_prime,Ta,Tm,Se,Sut)
+
 FOS_yield = local_yield_FOS_func(Sy,siga_prime,sigm_prime)
 end
 
