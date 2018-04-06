da8044f86dd35cd6f678377576a31e9d3a5c95c0
HAL9000\caroline <caitken5@uwo.ca>
2018-04-05 23:16:10 -0400

    Output Shaft Calculations

--- Project/Shaft Design/CallingTestShaft.m
+++ Project/Shaft Design/CallingTestShaft.m
@@ -9,7 +9,7 @@ Sut=1250;
 %  4 - As-forged
 finish=2;
 % d - diameter in mm
-d=25;
+d=10;
 % loading
 % 1 - bending or mixed
 % 2 - axial
@@ -22,7 +22,7 @@ reliability=0.9;
 % Kt - look up in appendix A-15
 Kt_bend=1.3;
 % q - lookup on page 303 shigely
-q_bend=0.9;
+q_bend=0.9325;
 % Kt - look up in appendix A-15
 Kt_axial=1;
 % q - lookup on page 303 shigely
@@ -32,14 +32,14 @@ Kts=1.2;
 % qs - lookup on page 304 shigely
 qs=0.95;
 % Max and Min bending moment at point
-sigma_bending_max=4.715;
-sigma_bending_min=-4.715;
+sigma_bending_max=3.689;
+sigma_bending_min=-3.689;
 % Max and min axial force at point
 sigma_axial_max=0;
 sigma_axial_min=0;
 % Max and Min torque at point
-torque_max=11.468;
-torque_min=11.468;
+torque_max=57.98;
+torque_min=57.98;
 % Yield Strength
 Sy=1050;
 [FOS,FOS_torsion,FOS_yield] = TestShaft(Sut,finish,d,loading,temp,reliability,Kt_bend,q_bend,Kt_axial,q_axial,Kts,qs,sigma_bending_max,sigma_bending_min,sigma_axial_max,sigma_axial_min,torque_max,torque_min,Sy);
--- Project/Shaft Design/SHAFTSTARTHERE.m
+++ Project/Shaft Design/SHAFTSTARTHERE.m
@@ -9,7 +9,7 @@ Sut=1250;
 %  4 - As-forged
 finish=2;
 % d - diameter in mm
-d=25;
+d=10;
 % loading
 % 1 - bending or mixed
 % 2 - axial
@@ -20,26 +20,26 @@ temp=25;
 % R - reliability as a decimal (ex 90% = 0.9)
 reliability=0.9;
 % Kt - look up in appendix A-15
-Kt_bend=1.65;
+Kt_bend=1.3;
 % q - lookup on page 303 shigely
-q_bend=0.95;
+q_bend=0.9325;
 % Kt - look up in appendix A-15
 Kt_axial=1;
 % q - lookup on page 303 shigely
 q_axial=1;
 % Kts - look up in appendix A-15 (16?)
-Kts=1.375;
+Kts=1.2;
 % qs - lookup on page 304 shigely
-qs=0.95;
+qs=0.945;
 % Max and Min bending moment at point
-sigma_bending_max=0.1014;
-sigma_bending_min=-0.1014;
+sigma_bending_max=7.165;
+sigma_bending_min=-7.165;
 % Max and min axial force at point
 sigma_axial_max=0;
 sigma_axial_min=0;
 % Max and Min torque at point
-torque_max=12.07;
-torque_min=12.07;
+torque_max=57.98;
+torque_min=57.98;
 % Yield Strength
 Sy=1050;
 [FOS,FOS_torsion,FOS_yield] = shafts(Sut,finish,d,loading,temp,reliability,Kt_bend,q_bend,Kt_axial,q_axial,Kts,qs,sigma_bending_max,sigma_bending_min,sigma_axial_max,sigma_axial_min,torque_max,torque_min,Sy);
